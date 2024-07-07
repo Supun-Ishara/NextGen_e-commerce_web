49e95efd04018d1aab864167c64d22c4e2bc3717
Supun Jayathilaka <147173476+Supun-Ishara@users.noreply.github.com>
2024-07-07 09:11:45 +0530

    started up

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const dbConnect = require("./config/dbConnect");
+const app = express();
+const dotenv = require("dotenv").config();
+const PORT = process.env.PORT || 4000;
+const authRouter = require("./routes/authRoute");
+dbConnect();
+
+// app.use('/', (req, res) => {
+//     res.send("Hello from server side");
+// });
+
+app.use('/api/user', authRouter);
+
+app.listen(PORT, () => {
+    console.log(`Server is running at port ${PORT}`);
+});
