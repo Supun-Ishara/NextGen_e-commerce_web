39d5fa5409822f18ea0b9d5621b75d542c484a8a
Supun Jayathilaka <147173476+Supun-Ishara@users.noreply.github.com>
2024-07-11 11:41:52 +0530

    new changes

--- controller/userCtrl.js
+++ controller/userCtrl.js
@@ -2,10 +2,10 @@ const User = require("../models/userModel");
 
 const createUser = async (req, res) => {
     const email = req.body.email;
-    const findUser = await User.findOne(email);
+    const findUser = await User.findOne({email:email});
     if (!findUser) {
        //Create a new User
-       const newUser = User.create(req.body);
+       const newUser = await User.create(req.body);
        res.json(newUser);
     } else {
         res.json({
--- index.js
+++ index.js
@@ -4,7 +4,11 @@ const app = express();
 const dotenv = require("dotenv").config();
 const PORT = process.env.PORT || 4000;
 const authRouter = require("./routes/authRoute");
+const bodyParser = require("body-parser");
 dbConnect();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false }));
 
 // app.use('/', (req, res) => {
 //     res.send("Hello from server side");
