36d346deb70bdd5ffa3b9f3d980587a11f7129d0
Supun Jayathilaka <147173476+Supun-Ishara@users.noreply.github.com>
2024-09-01 22:04:46 +0530

    Created the blog

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const authRouter = require("./routes/authRoute");
 const { notFound, errorHandler } = require("./middlewares/errorHandler");
 const bodyParser = require("body-parser"); 
 const productRouter = require("./routes/productRoute");
+const blogRouter = require("./routes/blogRoute");
 const cookieParser = require("cookie-parser");
 const morgan = require("morgan");
 dbConnect();
@@ -23,6 +24,7 @@ app.use(cookieParser());
 
 app.use('/api/user', authRouter);
 app.use('/api/product', productRouter);
+app.use('/api/blog', blogRouter);
 
 app.use(notFound);
 app.use(errorHandler);
