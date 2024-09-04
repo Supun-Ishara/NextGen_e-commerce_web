7db4222668b4f809cb662864f40d3d5087270e5b
Supun Jayathilaka <147173476+Supun-Ishara@users.noreply.github.com>
2024-09-04 22:29:14 +0530

    Created coupon

---
+++ routes/couponRoute.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const { isAdmin, authMiddleware } = require("../middlewares/authMiddleware");
+const { createCoupon, getAllCoupons, updateCoupon, deleteCoupon } = require("../controller/couponCtrl");
+const router = express.Router();
+
+router.post("/", authMiddleware, isAdmin, createCoupon);
+router.get("/", authMiddleware, isAdmin, getAllCoupons);
+router.put("/:id", authMiddleware, isAdmin, updateCoupon);
+router.delete("/:id", authMiddleware, isAdmin, deleteCoupon);
+
+module.exports = router;
